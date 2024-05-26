592334c52fb516dd11e974d681bdeff1b767ed0c
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 07:09:53 +0200

    Enhanced survey UI with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import './App.css';
 import SurveySection from './components/SurveySection';
--- src/components/SurveyStatement.js
+++ src/components/SurveyStatement.js
@@ -1,5 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-
 function SurveyStatement({ statement }) {
   return (
     <div className="survey-statement">
