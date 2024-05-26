f47cb50b9b8b7ff7fd3519e445d3432466b67a29
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 17:13:19 +0200

    Updated Version

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <img src={`${process.env.PUBLIC_URL}/EssHeader5.png`}/>
+      <img src={`${process.env.PUBLIC_URL}/EssHeader5.png`} alt="Header" />
+      <div className="header-gradient"></div>
     </div>
   );
 };
