c13cc2c5855953fa7af96907d847a2012701d4ee
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 13:32:33 +0200

    Background

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../assets/header-logo.png'; // Replace with your actual image path
 
-function Header() {
+const Header = () => {
   return (
-    <header className="header">
-      <img src={logo} alt="Header Logo" className="header-logo" />
-    </header>
+    <div className="header">
+      <img src={`${process.env.PUBLIC_URL}/EssHeader.png`}/>
+    </div>
   );
-}
+};
 
 export default Header;
