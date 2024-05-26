bdc7330fad0eec8d9ffb8ddc26aac11a9ff733d2
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 12:52:58 +0200

    Updated Version

--- src/App.js
+++ src/App.js
@@ -66,8 +66,85 @@ const survey = [
       "I receive praise and appreciative feedback from my manager when I do good work.",
       "My manager is able to inspire me with the ideas and targets of the company."
     ]
+  },
+  {
+    heading: "HPO Team",
+    statements: [
+      "In my area of work, continuing education and knowledge-sharing ensure that the necessary expertise is available."
+    ]
+  },
+  {
+    heading: "Quality",
+    statements: [
+      "In my area of work, we rely on expert knowledge to ensure the highest quality.",
+      "Processes in my area of work are continuously being improved.",
+      "In my area of work, mistakes are viewed as an opportunity to learn and improve.",
+      "In my area of work, we make sure BMW Group products achieve the highest quality.",
+      "In my area of work, decisions and actions are systematically geared toward creating value for internal customers."
+    ]
+  },
+  {
+    heading: "Digitalization",
+    statements: [
+      "In my area of work, the advantages of digitalization are used to best effect."
+    ]
+  },
+  {
+    heading: "Sustainability",
+    statements: [
+      "In my opinion, the BMW Group accepts its responsibility towards society and considers social matters.",
+      "I feel a responsibility to actively contribute to the BMW Group's sustainability goals.",
+      "The BMW Group sets itself ambitious environmental and climate-protection goals and acts in accordance with them."
+    ]
+  },
+  {
+    heading: "Employee Engagement",
+    statements: [
+      "I am proud to work for the BMW Group.",
+      "I would recommend the BMW Group as an employer.",
+      "At the BMW Group, I feel that my work is important."
+    ]
+  },
+  {
+    heading: "Health",
+    statements: [
+      "In my area of work, I can work in a healthy and efficient manner.",
+      "At the BMW Group, I feel that my work is important.",
+      "My work and private life are well-balanced.",
+      "My workload is generally manageable.",
+      "Image & Employer Branding",
+      "I am proud to work for the BMW Group.",
+      "I am optimistic regarding the future of the BMW Group.",
+      "I receive appropriate compensation for my work and my performance.",
+      "The BMW Group provides very good benefits (social and additional).",
+      "In my opinion, the BMW Group accepts its responsibility towards society and considers social matters."
+    ]
+  },
+  {
+    heading: "Working Environment",
+    statements: [
+      "I can participate in decisions relating to my work (e.g. working time, place of work, ways of working).",
+      "Conditions in my area of work help me manage my work successfully (e.g. break rooms, areas for concentrated working, cooperation, ergonomics, and indoor climate).",
+      "I find the environment at my place of work attractive (e.g. location, accessibility, parking, food services)."
+    ]
+  },
+  {
+    heading: "Diversity",
+    statements: [
+      "My manager develops the skills of all team members, regardless of differences in gender, age, professional background, nationality, etc.",
+      "At the BMW Group, a diverse workforce is seen as a strength."
+    ]
+  },
+  {
+    heading: "Compliance",
+    statements: [
+      "At the BMW Group, compliance with laws, internal regulations and compliance provisions play an important role.",
+      "Responsible and lawful conduct is modelled and actively promoted by managers.",
+      "Follow-up Process",
+      "In my area of work, the results of the last Employee Survey were discussed in detail.",
+      "In my area of work, concrete measures were developed from the results of the last Employee Survey and implemented."
+    ]
   }
-  // Add more sections and statements as needed
 ];
 
 function App() {
