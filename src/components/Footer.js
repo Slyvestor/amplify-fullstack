28292cfa553736e4433116a9efba13f13ce3f7e5
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 17:02:42 +0200

    Updated Version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>&copy; 2024 BMW Group. All rights reserved.</p>
-      <p>Developed by Sylvestor Sehlare Im Auftrag von / by order of TX-R-1-S</p>
-    </footer>
+    <div className="footer">
+      <img src={`${process.env.PUBLIC_URL}/Footer.png`} alt="Footer" />
+    </div>
   );
 };
 
