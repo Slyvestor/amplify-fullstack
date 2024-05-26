88c0e490d656979c126b463b3bb8b7c38edd8f39
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 13:00:08 +0200

    Updated Version

--- src/components/SurveySection.js
+++ src/components/SurveySection.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SurveyStatement from './SurveyStatement';
-import { Collapse } from 'react-bootstrap';
 
 function SurveySection({ section }) {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="survey-section mb-4">
-      <h2
-        className="h4 mb-3"
-        onClick={() => setOpen(!open)}
-        style={{ cursor: 'pointer' }}
-        aria-controls={`collapse-${section.heading}`}
-        aria-expanded={open}
-      >
-        {section.heading}
-      </h2>
-      <Collapse in={open}>
-        <div id={`collapse-${section.heading}`}>
-          {section.statements.map((statement, index) => (
-            <SurveyStatement key={index} statement={statement} />
-          ))}
-        </div>
-      </Collapse>
+      <h2 className="h4 mb-3">{section.heading}</h2>
+      <div>
+        {section.statements.map((statement, index) => (
+          <SurveyStatement key={index} statement={statement} />
+        ))}
+      </div>
     </div>
   );
 }
