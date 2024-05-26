c9873c03932517e19fd4a614913d2cc0437d7134
Sehlare Sylvestor <qxz543k@africa.bmw.corp>
2024-05-26 13:07:55 +0200

    Updated Version

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <img src={`${process.env.PUBLIC_URL}/EssHeader.png`} alt="ESS Feedback Header" className="img-fluid" />
+      <img src={`${process.env.PUBLIC_URL}/EssHeader.png`}/>
     </div>
   );
 };
